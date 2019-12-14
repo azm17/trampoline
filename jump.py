20d3a2b88bdaf9b9d885691e449d7d9f216c6a05
azm17 <azumi_azumi0917@yahoo.co.jp>
2019-12-15 04:38:25 +0900

    1225

--- jump.py
+++ jump.py
@@ -2,13 +2,13 @@
 
 import sys
 import pygame
-#import random
-from pygame.locals import QUIT, KEYDOWN, K_ESCAPE,K_SPACE, K_r
+import random
+from pygame.locals import QUIT, KEYDOWN, K_ESCAPE,K_SPACE,K_2,K_1
 import android_3type_pic as my_func
-
+import matplotlib.pyplot as plt
 
 # ===== Physical parameters =====
-FPS = 30     # フレームレート [f/s]
+FPS = 30    # フレームレート [f/s]
 DT = 1 / FPS # ステップ時間 [s]
 G = 9.8      # 重力加速度 [kg.m/s^2]
 K = 10       # ばね定数 [N/m]
@@ -22,6 +22,13 @@ def first_message(sf, msg, h, color):
     sf.blit(message, message_rect)
     pygame.time.delay(5)
     pygame.display.update()
+
+def info_message(sf, msg, h, color):
+    sysfont = pygame.font.SysFont(None, 30)
+    message = sysfont.render(msg, True, color)
+    message_rect = message.get_rect()
+    message_rect.center = (50,h)
+    sf.blit(message, message_rect)
 
 class MyObject():
     def __init__(self, x, y, size_vertical, size_horizontal, name, draw_mode):
@@ -59,24 +66,21 @@ class Android(MyObject):# 主人公
     def __init__(self, x, y, sv, sh, name, draw_mode):
         super(Android, self).__init__(x, y, sv, sh, name, draw_mode)
         self.time = 0# 時間
-        self.weight = 10# 重さ　
+        self.weight = 20# 重さ　
         self.velocity = 0# 速度
         self.start_position = y
-        my_func.create_jumping_android(14)#画像生成
-        my_func.create_crouching_android(14)
-        my_func.create_falling_android(14)
+        self.n_jump = 0
         self.image = [pygame.image.load("./jumping_android1.png"),
                       pygame.image.load("./crouching_android1.png"),#しゃがんでいるとき用1
                       pygame.image.load("./falling_android1.png")]#落ちるとき用2
     
     def draw(self, surface, x):
+        if self.velocity > 0:
+            form = 2
+        else: 
+            form = 0
         if x > 0:
             form = 1
-        else:
-            if self.velocity > 0:
-                form = 2
-            else: 
-                form = 0
         
         # 矩形を描画 pygame.draw.rect(Surface, color, Rect, width=0)
         if self.draw_mode == 0 or self.draw_mode == 2:
@@ -90,11 +94,14 @@ class Android(MyObject):# 主人公
                           +f'Velocity: {round(self.velocity,1)}\n'\
                           +f'Velocity: {round(self.velocity,1)}\n')
 
-# 描画
+# 描画処理
 def draw(surface, ground, android):
     surface.fill((0,0,0))# 画面初期化
-    pygame.time.delay(5)
-    x = android.y + android.size_vertical - ground.y        
+    #pygame.time.delay(1)
+    x = android.y + android.size_vertical - ground.y
+    # オブジェクト表示
+    android.draw(surface, x)
+    ground.draw(surface)
     if x > 0:
         pygame.draw.line(surface, (255, 255, 0), 
                          (ground.x, ground.y), 
@@ -107,66 +114,132 @@ def draw(surface, ground, android):
         pygame.draw.line(surface, (255, 255, 0), 
                          (android.x, android.y + android.size_vertical), 
                          (android.x + android.size_horizontal, android.y + android.size_vertical), 2)
-    # オブジェクト表示
-    #for p in obgect_list:
-    android.draw(surface, x)
-    ground.draw(surface)
     # スタート位置
     pygame.draw.line(surface, (255, 255, 255), 
                      (115,android.start_position),
                      (215,android.start_position), 1)
     # 頭の位置
     pygame.draw.line(surface, (255, 255, 255), (115,android.y), (215,android.y), 1)
+    
+    #msg=f"Height: {round(android.y,1)}"
+    info_message(surface, "Number:", 10, (255,255,255))
+    info_message(surface, f"{trial}", 30, (255,255,255))
+    info_message(surface, "Time:", 50, (255,255,255))
+    info_message(surface, f"{round(android.time)}", 70, (255,255,255))
+    info_message(surface, "Height:", 90, (255,255,255))
+    info_message(surface, f"{round(-(android.y-ground.y))}", 110, (255,255,255))
+    info_message(surface, "Jump:", 130, (255,255,255))
+    info_message(surface, f"{round(android.n_jump,1)}", 150, (255,255,255))
+    
+    
+    
     pygame.display.update()
-
-def calculation(android, ground):
+    
+# 物理計算
+def calculation(android, ground, Apply_force_down_t):
+    global jump
+    global tmp_t
+    
     f = android.weight * G # 重力
     x = android.y + android.size_vertical - ground.y# トランポリンの表面との距離
-    # トランポリンに衝突したとき
-    if x > 0:
+    if x > 0:# トランポリンに衝突したとき
+        tmp_t += DT
         f += -K * x
-        #f += -C * android.velocity# 減衰
-    
+        #f += -C * android.velocity# 減衰        
+        if jump and tmp_t >= Apply_force_down_t:# ジャンプする
+            f += 10*android.weight * G
+            #print(f'T_J: {tmp_t}\nAFD: {Apply_force_down_t}\n')
+            jump = False
+            android.n_jump+=1
+    else:
+        tmp_t = 0
+        jump = True
     # 動きの計算
     alpha = f / android.weight# a = f / m （ニュートンの運動方程式），加速度を求める
     android.velocity += alpha * DT# v += a * dt （積分：加速度 ⇒ 速度），速度を計算
     android.y += android.velocity * DT# y += v * dt (積分：速度 ⇒ 位置)，次の位置を計算
     android.time += DT# 経過時間
 
+# ボタン処理
 def button():
     global state
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit(); sys.exit()
-            
         if event.type == KEYDOWN:# 終了
             if event.key == K_ESCAPE:
                 pygame.quit(); sys.exit()
-            if event.key == K_SPACE:# 計算開始
+            if event.key == K_2:
+                state = 2
+            if event.key == K_SPACE or event.key == K_1:# 計算開始
                 state = 1
 
+def draw_graph(surface,data):
+    surface.fill((0,0,0))# 画面初期化
+    surface.blit(pygame.image.load("./figure.png"), (20,20))
+    pygame.display.update()
+
+def opt():
+    return random.random()*10
+
 def main():
+    global jump # ジャンプできるかできないかTrue or False
     global state # ゲームの状態:{開始前:0,実行中:1}
-    width_stage = 300; height_stage = 600# area設定
+    global tmp_t
+    global trial
+    trial = 0# 試行回数
+    jump = True
+    
+    my_func.create_jumping_android(14)#画像生成
+    my_func.create_crouching_android(14)
+    my_func.create_falling_android(14)
+        
+    width_stage = 500; height_stage = 600# area設定
     pygame.init()
     pygame.display.set_mode((width_stage, height_stage)) # 画面設定
     pygame.display.set_caption("トランポリン") #タイトル
     surface = pygame.display.get_surface()
     surface.fill((0,0,0)) # 画面初期化
     
-    state = 0# 初期状態
-    android = Android(150, 100, 40, 40, 'android', 1)# ドロイド君生成，位置(x,y)=(150, 100),大きさ(40*40)
-    ground = Ground(115, 450, 10, 100, 'ground', 0)# ground
+    state = 1# 初期状態    
+    data = []
     
+    ground = Ground(115, 450, 10, 100, 'ground', 0)# ground
+    trial = 0
     while(True):
-        button()# ボタン処理
-        if state == 0:
-            first_message(surface, "Hit space Key", 150, (255,0,0))
+        y_max = -10000
+        android = Android(150, 100, 40, 40, 'android', 1)# ドロイド君生成，位置(x,y)=(150, 100),大きさ(40*40)
+        Apply_force_down_t = 1# ジャンプするタイミングによって，y_maxを最適化する．y_maxは10回目の頂点の高さ
         
-        if state == 1:
-            calculation(android, ground)# 物理計算
-            android.print_details()# デバッグ
-            draw(surface, ground, android)# 描画処理
+        while(True):
+            button()# ボタン処理
+            if state == 0:
+                first_message(surface, "Hit space Key", 150, (255,0,0))
+            if state == 1 or 2:
+                calculation(android, ground, Apply_force_down_t)# 物理計算
+            if state == 1:
+                #android.print_details()# デバッグ
+                draw(surface, ground, android)# 描画処理
+            if android.n_jump > 10 and jump and android.velocity > 0:
+                tmp_y = -(android.y-ground.y)
+                if y_max < tmp_y:
+                   y_max = tmp_y 
+                break
+        #y_max
+        trial += 1#試行回数
+        data.append(y_max)
+        if state == 2 and trial%10==0:
+            plt.figure()
+            plt.plot(data)
+            plt.xlabel("N")
+            plt.ylabel("Height")
+            plt.title("GA")
+
+            plt.savefig('figure.png')
+            plt.close()
+
+            draw_graph(surface,data)
         
+
 if __name__ == "__main__":
     main()
