f092f7f1f481b8be0c678bf8f041736a2d45b985
azm17 <azumi_azumi0917@yahoo.co.jp>
2019-12-16 15:10:59 +0900

    sss

---
+++ ga2.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 15 20:41:50 2019
+
+@author: Azumi Mamiya
+"""
+
+import matplotlib.pyplot as plt
+import random
+import copy
+import jump as jump
+
+class SimpleGA:
+    def __init__(self):
+        self.N = 100# 染色体の数
+        self.ITERATION = 100# 繰り返し
+        self._initialize()
+        self.average = []
+
+    def _initialize(self):# 染色体を生成
+        self.pool = [Chromosome() for i in range(self.N)]
+
+    def _mutateAll(self):# 突然変異を起こす
+        for i in range(self.N):
+            self.pool[i].mutate()
+
+    def _tournamentSelection(self):# トーナメント選択
+        pool_next = []
+        while len(pool_next) < self.N:
+            offspring1 = copy.deepcopy(self.pool[random.randrange(self.N)])#ランダムで遺伝子をコピー
+            offspring2 = copy.deepcopy(self.pool[random.randrange(self.N)])#ランダム遺伝子をでコピー
+            if offspring1.getFittness() > offspring2.getFittness():#offspring1の適応度が高いとき
+                pool_next.append(offspring1)#offspring1を追加
+            else:#offspring2の適応度が高いとき
+                pool_next.append(offspring2)#offspring2を追加
+        self.pool = pool_next[:]
+
+    def _rouletteSelection(self):# ルーレット選択
+        # ルーレット選択の関数
+        # ここを実装する
+        pool_next = []
+        total = 0
+        for pool_i in self.pool:
+            total += pool_i.getFittness()
+        
+        while (len(pool_next) < self.N):
+            sum_fittness = 0
+            p = random.random()
+            for pool_i in self.pool:
+                sum_fittness += pool_i.getFittness()
+                if p <= sum_fittness/total:
+                    pool_next.append(copy.deepcopy(pool_i))
+                    break
+        self.pool = pool_next[:]
+
+    def _printStatus(self, iteration):# 
+        print("generation\t" + str(iteration))# iteration: 繰り返し回数
+        for c in self.pool:
+            print("\t" + str(c))
+
+    def _printAverage(self):
+        # 適応度の平均値を計算する関数
+        # ここを実装し，平均適応度の時間変化のグラフに利用する
+        sum = 0
+        for p in self.pool:
+            sum += p.getFittness()
+        average = sum/len(self.pool)
+        #print("Average\t ", average)
+        self.average.append(average)
+    
+    def create_fig(self):
+        plt.plot(self.average, color='blue')
+        plt.ylim(0,1)
+        plt.ylabel('Average Fitness')
+        plt.xlabel('Generation')
+        
+    def evolve(self):
+        for i in range(self.ITERATION):
+            #self._printStatus(i)
+            self._printAverage()
+            #self._tournamentSelection()
+            self._rouletteSelection()
+            self._mutateAll()
+            
+            self.create_fig()
+            
+class Chromosome:
+    def __init__(self):
+        self.LENGTH = 8# 長さ
+        self.MUTATION_RATE = 0.05# 突然変異率
+        self.gene = [random.randint(0,1) for i in range(self.LENGTH)]# 遺伝子
+
+    def setGene(self, new_gene):
+        self.gene = new_gene[:]
+
+    def mutate(self):# 突然変異
+        for i in range(self.LENGTH):
+            if random.random() < self.MUTATION_RATE:
+                self.gene[i] = 1 - self.gene[i]
+
+    def getFittness(self):# 適応度の計算
+        #value = 0.
+        #for g in self.gene:
+        #    value *= 2
+        #    value += g
+        #result = value / (2 ** self.LENGTH - 1.0)
+        # return result * result
+        ground = jump.Ground(115, 450, 10, 100, 'ground', 0)# ground
+        android = jump.Android(150, 300, 40, 40, 'android', 1)
+        Apply_force_down_t = 2
+        
+        jump.calculation(android, ground, Apply_force_down_t)# 物理計算
+        return result
+
+    def __str__(self):
+        result = ""
+        for g in self.gene:
+            result += str(g)
+        result += "\t" + str(self.getFittness())
+        return result
+
+ga = SimpleGA()
+ga.evolve()
--- jump2.py
+++ jump2.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import sys
 import pygame
 import random
@@ -8,14 +7,12 @@ import android_3type_pic as my_func
 import matplotlib.pyplot as plt
 import copy
 
-
 # ===== Physical parameters =====
 FPS = 30    # フレームレート [f/s]
 DT = 1 / FPS # ステップ時間 [s]
 G = 9.8      # 重力加速度 [kg.m/s^2]
 K = 10       # ばね定数 [N/m]
 C = 0.4
-
 def first_message(sf, msg, h, color):
     sysfont = pygame.font.SysFont(None, 60)
     message = sysfont.render(msg, True, color)
@@ -24,14 +21,12 @@ def first_message(sf, msg, h, color):
     sf.blit(message, message_rect)
     pygame.time.delay(5)
     pygame.display.update()
-
 def info_message(sf, msg, h, color):
     sysfont = pygame.font.SysFont(None, 30)
     message = sysfont.render(msg, True, color)
     message_rect = message.get_rect()
     message_rect.center = (50,h)
     sf.blit(message, message_rect)
-
 class MyObject():
     def __init__(self, x, y, size_vertical, size_horizontal, name, draw_mode):
         # player settings
@@ -41,29 +36,24 @@ class MyObject():
         self.size_vertical = size_vertical# オブジェクトの大きさ
         self.size_horizontal = size_horizontal
         self.draw_mode = draw_mode
-    
     def xy_body(self):# オブジェクトの範囲
         return {'x1':self.x,
                 'x2':self.x + self.size_horizontal,
                 'y1':self.y,
                 'y2':self.y + self.size_vertical}
-    
     def print_details(self):
         print(f"--------\nPosition: ({self.x},{self.y})")
-
 class Ground(MyObject):# トランポリン
     def __init__(self, x, y, sv, sh, name, draw_mode):
         super(Ground, self).__init__(x, y, sv, sh, name, draw_mode)
-    
     def draw(self, surface):
         # 矩形を描画 pygame.draw.rect(Surface, color, Rect, width=0)
         #if mode==0 or mode == 2:
-        pygame.draw.rect(surface, 
-                         (255, 255, 0), 
+        pygame.draw.rect(surface,
+                         (255, 255, 0),
                          (self.x, self.y, self.size_horizontal, self.size_vertical))
         #if mode==1 or mode == 2:
         #    surface.blit(image, (self.x-22,self.y-10))
-        
 class Android(MyObject):# 主人公
     def __init__(self, x, y, sv, sh, name, draw_mode):
         super(Android, self).__init__(x, y, sv, sh, name, draw_mode)
@@ -75,27 +65,23 @@ class Android(MyObject):# 主人公
         self.image = [pygame.image.load("./jumping_android1.png"),
                       pygame.image.load("./crouching_android1.png"),#しゃがんでいるとき用1
                       pygame.image.load("./falling_android1.png")]#落ちるとき用2
-    
     def draw(self, surface, x):
         if self.velocity > 0:
             form = 2
-        else: 
+        else:
             form = 0
         if x > 0:
             form = 1
-        
         # 矩形を描画 pygame.draw.rect(Surface, color, Rect, width=0)
         if self.draw_mode == 0 or self.draw_mode == 2:
             pygame.draw.rect(surface, (0, 0, 255), (self.x, self.y, self.size_vertical, self.size_horizontal))
         if self.draw_mode == 1 or self.draw_mode == 2:
             surface.blit(self.image[form], (self.x-22,self.y-10))
-    
     def print_details(self):
         print(f'----time:{round(self.time,1)}----\n'\
                           +f'Position: ({round(self.x,)},{round(self.y,1)})\n'\
                           +f'Velocity: {round(self.velocity,1)}\n'\
                           +f'Velocity: {round(self.velocity,1)}\n')
-
 # 描画処理
 def draw(surface, ground, android):
     surface.fill((0,0,0))# 画面初期化
@@ -105,24 +91,21 @@ def draw(surface, ground, android):
     android.draw(surface, x)
     ground.draw(surface)
     if x > 0:
-        pygame.draw.line(surface, (255, 255, 0), 
-                         (ground.x, ground.y), 
+        pygame.draw.line(surface, (255, 255, 0),
+                         (ground.x, ground.y),
                          (android.x, android.y + android.size_vertical), 2)
-        
-        pygame.draw.line(surface, (255, 255, 0), 
-                         (ground.x + ground.size_horizontal-3, ground.y), 
+        pygame.draw.line(surface, (255, 255, 0),
+                         (ground.x + ground.size_horizontal-3, ground.y),
                          (android.x + android.size_horizontal, android.y + android.size_vertical), 2)
-        
-        pygame.draw.line(surface, (255, 255, 0), 
-                         (android.x, android.y + android.size_vertical), 
+        pygame.draw.line(surface, (255, 255, 0),
+                         (android.x, android.y + android.size_vertical),
                          (android.x + android.size_horizontal, android.y + android.size_vertical), 2)
     # スタート位置
-    pygame.draw.line(surface, (255, 255, 255), 
+    pygame.draw.line(surface, (255, 255, 255),
                      (115,android.start_position),
                      (215,android.start_position), 1)
     # 頭の位置
     pygame.draw.line(surface, (255, 255, 255), (115,android.y), (215,android.y), 1)
-    
     #msg=f"Height: {round(android.y,1)}"
     info_message(surface, "Number:", 10, (255,255,255))
     info_message(surface, f"{trial}", 30, (255,255,255))
@@ -132,33 +115,28 @@ def draw(surface, ground, android):
     info_message(surface, f"{round(-(android.y-ground.y))}", 110, (255,255,255))
     info_message(surface, "Jump:", 130, (255,255,255))
     info_message(surface, f"{round(android.n_jump,1)}", 150, (255,255,255))
-    
-    
-    
     pygame.display.update()
-    
 # 物理計算
 def calculation(android, ground, Apply_force_down_t):
     global jump
-    global jump2
     global tmp_t
-    
+    global jump2
     f = android.weight * G # 重力
     x = android.y + android.size_vertical - ground.y# トランポリンの表面との距離
-    
     if x > 0:# トランポリンに衝突したとき
         tmp_t += DT
         f += -K * x
-        #f += -C * android.velocity# 減衰        
+        #f += -C * android.velocity# 減衰
         if jump and tmp_t >= Apply_force_down_t:# ジャンプする
             f += 2*android.weight * G
             #print(f'T_J: {tmp_t}\nAFD: {Apply_force_down_t}\n')
             jump = False
         if jump2:
-            android.n_jump += 1
-            jump = False
+            #print("AAA")
+            android.n_jump+=1
+            jump2=False
     else:
-        jump2 = True
+        jump2=True
         tmp_t = 0
         jump = True
     # 動きの計算
@@ -166,7 +144,6 @@ def calculation(android, ground, Apply_force_down_t):
     android.velocity += alpha * DT# v += a * dt （積分：加速度 ⇒ 速度），速度を計算
     android.y += android.velocity * DT# y += v * dt (積分：速度 ⇒ 位置)，次の位置を計算
     android.time += DT# 経過時間
-
 # ボタン処理
 def button():
     global state
@@ -180,27 +157,10 @@ def button():
                 state = 2
             if event.key == K_SPACE or event.key == K_1:# 計算開始
                 state = 1
-
 def draw_graph(surface,data):
     surface.fill((0,0,0))# 画面初期化
     surface.blit(pygame.image.load("./figure.png"), (20,20))
     pygame.display.update()
-    
-def _rouletteSelection(self):# ルーレット選択
-        pool_next = []
-        total = 0
-        for pool_i in self.pool:
-            total += pool_i.getFittness()
-        
-        while (len(pool_next) < self.N):
-            sum_fittness = 0
-            p = random.random()
-            for pool_i in self.pool:
-                sum_fittness += pool_i.getFittness()
-                if p <= sum_fittness/total:
-                    pool_next.append(copy.deepcopy(pool_i))
-                    break
-        self.pool = pool_next[:]
 
 def main():
     global jump # ジャンプできるかできないかTrue or False
@@ -214,72 +174,57 @@ def main():
     my_func.create_jumping_android(14)#画像生成
     my_func.create_crouching_android(14)
     my_func.create_falling_android(14)
-        
-    width_stage = 500; height_stage = 600# area設定
+    width_stage = 1000; height_stage = 600# area設定
     pygame.init()
     pygame.display.set_mode((width_stage, height_stage)) # 画面設定
     pygame.display.set_caption("トランポリン") #タイトル
     surface = pygame.display.get_surface()
     surface.fill((0,0,0)) # 画面初期化
-    
-    state = 2# 初期状態    
+    state = 0# 初期状態
     data = []
-    
     ground = Ground(115, 450, 10, 100, 'ground', 0)# ground
     trial = 0
-    N = 100
-        # ジャンプするタイミングによって，y_maxを最適化する．y_maxは10回目の頂点の高さ
-    Apply_force_down_t = [random.uniform(0,3) for i in range(N)]
-    Apply_force_down_t = [2.9 for i in range(N)]
-    
+    N = 1000
+    AFD=[]
+    for i in range(N):
+        AFD.append(random.uniform(0,6))
+    #AFD = [random.uniform(0,10) for i in range(N)]
     while(True):
-        #y_max = -10000
+        y_max = -10000
         android = Android(150, 300, 40, 40, 'android', 1)# ドロイド君生成，位置(x,y)=(150, 100),大きさ(40*40)
-        # Apply_force_down_t = [3 for i in range(N)]
-        
-        y_max_list = []
-        for AFD in Apply_force_down_t:
-            y_max = -10000
-            while(True):# 1人ずつの処理
+        y_max_list=[]
+        for Apply_force_down_t in AFD:
+            while(True):
                 button()# ボタン処理
                 if state == 0:
                     first_message(surface, "Hit space Key", 150, (255,0,0))
-                if state == 1 or state == 2:
-                    calculation(android, ground, AFD)# 物理計算
+                if state == 1 or 2:
+                    calculation(android, ground, Apply_force_down_t)# 物理計算
                 if state == 1:
                     #android.print_details()# デバッグ
                     draw(surface, ground, android)# 描画処理
-                if android.n_jump > 10 and jump and android.velocity > 0:
+                if android.n_jump > 10 and jump2 and android.velocity > 0:
                     tmp_y = -(android.y-ground.y)
                     if y_max < tmp_y:
                        y_max = tmp_y
                     break
             y_max_list.append(y_max)
-        for i in range(100):
-            if Apply_force_down_t[i]>2:
-                print(Apply_force_down_t[i], y_max_list[i])
+        #print(y_max)
+        # y_max
         trial += 1#試行回数
-        #if trial%50 == 0:
-        #    print(Apply_force_down_t)
-        
-        interval = 5
-        if trial%interval == 0:
-            data.append(sum(y_max_list)/len(y_max_list))
-        if state == 2 and trial%interval == 0:
+        data.append(sum(y_max_list)/len(y_max_list))
+        if state == 2 and trial%10==0:
             plt.figure()
             plt.plot(data)
-            plt.xlabel("Number of trials")
+            plt.xlabel("N")
             plt.ylabel("Height")
-            plt.ylim(100,200)
-            #plt.ylim(100,:)
             plt.title("GA")
-
             plt.savefig('figure.png')
             plt.close()
-
             draw_graph(surface,data)
         
-        # ルーレット選択
+        print(AFD[1],y_max_list[1])
+        
         pool_next = []
         total = sum(y_max_list)
         while (len(pool_next) < N):
@@ -289,13 +234,32 @@ def main():
             for pool_i in y_max_list:
                 sum_fittness += pool_i
                 if p <= sum_fittness / total:
-                    pool_next.append(Apply_force_down_t[i])
+                    pool_next.append(AFD[i])
                     break
                 i += 1
-        Apply_force_down_t = pool_next[:]
-        #Apply_force_down_t[random.randint(0,N-1)] = 0.1
-        #print(Apply_force_down_t)
-        
-
+        """
+        pool_next = []
+        next_list=[i for i in range(N)]
+        while len(pool_next) < N:
+            #offspring1=copy.deepcopy(self.pool[random.choices(next_list,weights=self.Glist)])
+            #pool_next.append(self.pool[random.choices(next_list,weights=self.Glist)])
+            a=random.choices(next_list,weights=AFD)
+            #print(a)
+            #print(self.pool[a[0]])
+            offspring1=copy.deepcopy(AFD[a[0]])
+            #pass
+            pool_next.append(offspring1)
+        """
+        """
+        pool_next = []
+        while len(pool_next) < N:
+            offspring1 = copy.deepcopy(AFD[random.randrange(N)])
+            offspring2 = copy.deepcopy(AFD[random.randrange(N)])
+            if offspring1 < offspring2:
+                pool_next.append(offspring1)
+            else:
+                pool_next.append(offspring2)
+        """
+        AFD = pool_next
 if __name__ == "__main__":
     main()
